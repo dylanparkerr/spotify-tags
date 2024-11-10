3b7fd7a9a60ac143e1b84d81a53f122bed5a5f45
dylan parker <dylanparkerr@gmail.com>
2024-11-09 23:00:32 -0500

    initial config

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
 func main() {
-	println("hello, world!")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("$HOME/repos/spotify-tags/")
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	msg := viper.Get("my.config")
+	fmt.Println(msg)
 }
